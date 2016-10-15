4ee2e0b740552035659480add569820f08e70ce5
VitaHub <hubenko_vitalii@mail.ua>
2016-10-15 23:30:44 +0300

    production.rb updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,9 +49,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  config.action_cable.mount_path = nil
+  # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://juncti0n.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://juncti0n.herokuapp.com', /https:\/\/juncti0n.herokuapp.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://juncti0n.herokuapp.com', /http:\/\/juncti0n.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
